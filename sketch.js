723c4ce5b004903341678503e5369b438db4217e
PrinceKumarDhoni <75629714+PrinceKumarDhoni@users.noreply.github.com>
2021-01-10 12:53:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var canvas;
+var music;
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+    createCanvas(500,500);
+
+    //create 4 different surfaces
+s
+
+
+    //create box sprite and give velocity
+
+}
+
+function draw() {
+    background('red');
+    //create edgeSprite
+
+
+
+    //add condition to check if box touching surface and make it box
+}
